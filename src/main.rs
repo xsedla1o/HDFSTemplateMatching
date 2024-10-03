8235d673a9041531e55c68595fde641737f1cf12
Ondrej Sedlacek <xsedla1o@stud.fit.vutbr.cz>
2024-10-03 16:36:40 +0200

    Rawdog it. Took `3m 55s`, `14s` in release.
    The datetime parsing makes the biggest difference.
    I don't understand the longer dev build time as well.

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,89 @@
-use chrono::{NaiveDateTime, TimeZone, Utc};
+use atoi::FromRadix10;
+use chrono::NaiveDate;
+use core::str;
 use std::collections::HashMap;
 use std::fs::File;
 use std::io::{self, BufRead, BufWriter, Write};
 use std::path::Path;
 
+fn seq_find_bytes(haystack: &[u8], needle: &[u8]) -> Option<usize> {
+    seq_find_bytes_from(haystack, needle, 0)
+}
+
+fn seq_find_bytes_from(haystack: &[u8], needle: &[u8], start_pos: usize) -> Option<usize> {
+    let mut i = start_pos;
+    while i + needle.len() <= haystack.len() {
+        let mut found = true;
+        for j in 0..needle.len() {
+            if haystack[i + j] != needle[j] {
+                found = false;
+                break;
+            }
+        }
+        if found {
+            return Some(i);
+        }
+        i += 1;
+    }
+    None
+}
+
+fn blk_to_i<T>(s: &[u8]) -> T
+where
+    T: FromRadix10 + std::ops::Neg<Output = T>,
+{
+    if s[4] == b'-' {
+        let (num, _endbyte) = T::from_radix_10(&s[5..]);
+        return -num;
+    }
+    let (num, _endbyte) = T::from_radix_10(&s[4..]);
+    num
+}
+
+fn decode_label(label: &u8) -> &'static [u8] {
+    match label {
+        0 => b"Normal",
+        1 => b"Anomaly",
+        _ => panic!("Unknown label"),
+    }
+}
+
 fn main() {
-    let templates: Vec<Vec<String>> = read_lines("./templates.csv")
+    let templates: Vec<Vec<Vec<u8>>> = read_lines("./templates.csv")
         .unwrap()
         .map_while(Result::ok)
         .map(|line| {
-            let parts: Vec<String> = line
+            let line_str = String::from_utf8(line).unwrap();
+            let parts: Vec<Vec<u8>> = line_str
                 .trim_end_matches(" ")
                 .trim_start_matches("<*>")
                 .trim_end_matches("<*>")
                 .split("<*>")
-                .map(|part| part.to_string())
+                .map(|part| part.as_bytes().to_owned())
                 .collect();
             parts
         })
         .collect();
-    let mut templates_by_freq: Vec<(usize, &Vec<String>, u64)> = templates
+    let mut templates_by_freq: Vec<(usize, &Vec<Vec<u8>>, u64)> = templates
         .iter()
         .enumerate()
         .map(|(i, t)| (i, t, 0))
         .collect();
 
-    let mut labels: HashMap<String, String> = HashMap::new();
+    let mut labels: HashMap<i64, u8> = HashMap::new();
     let mut lines = read_lines("labels.csv").unwrap();
     lines.next(); // skip the header
 
     for line in lines.map_while(Result::ok) {
-        let parts: Vec<&str> = line.split(',').collect();
+        let parts: Vec<&[u8]> = line.split(|&b| b == b',').collect();
         if parts.len() >= 2 {
-            let name = parts[0].to_string();
-            let label = parts[1].trim().to_string();
-            labels.insert(name, label);
+            let blk_num = blk_to_i(parts[0]);
+            let label = match parts[1][0] {
+                b'N' => 0,
+                b'A' => 1,
+                _ => panic!("Unknown label"),
+            };
+            labels.insert(blk_num, label);
         }
     }
 
@@ -62,9 +111,9 @@ fn main() {
                 let mut current_pos = 0;
                 let mut found = true;
                 for part in *template {
-                    match line.as_str()[current_pos..].find::<&str>(part) {
+                    match seq_find_bytes_from(&line, part, current_pos) {
                         Some(pos) => {
-                            current_pos += pos;
+                            current_pos = pos;
                             positions.push(current_pos);
                             current_pos += part.len();
                             positions.push(current_pos);
@@ -85,65 +134,85 @@ fn main() {
             positions.insert(0, 0);
             positions.push(line.len());
 
-            let mut blk_id: Option<&str> = None;
-            let mut extra_blk_ids: Vec<&str> = vec![];
+            if template_id == 0 {
+                eprintln!("{:?}", str::from_utf8(&line).unwrap());
+                eprintln!("{:?}", positions);
+                panic!("Template not found");
+            }
+
+            let mut blk_id: Option<&[u8]> = None;
+            let mut extra_blk_ids: Vec<&[u8]> = vec![];
             if template_id == 30 {
-                let mut ids = line[positions[4]..positions[5]].trim().split(" ");
-                blk_id = Some(ids.next().expect(&line));
+                let mut ids = line[positions[4]..positions[5]]
+                    .trim_ascii()
+                    .split(|c: &u8| *c == b' ');
+                blk_id = Some(ids.next().unwrap());
                 for id in ids {
                     extra_blk_ids.push(id);
                 }
             } else {
-                let blk_end_patterns: &[_] = &[' ', '.', '\''];
+                let blk_end_patterns: &[_] = b" .'";
                 for (i, j) in (0..positions.len())
                     .step_by(2)
                     .zip((1..positions.len()).step_by(2))
                 {
-                    let param: &str = &line[positions[i]..positions[j]];
-                    if let Some(p) = param.find("blk_") {
-                        blk_id = Some(param[p..].split(blk_end_patterns).next().unwrap());
+                    let param: &[u8] = &line[positions[i]..positions[j]];
+                    if let Some(p) = seq_find_bytes(param, b"blk_") {
+                        blk_id = Some(
+                            param[p..]
+                                .split(|c: &u8| blk_end_patterns.iter().any(|p: &u8| c == p))
+                                .next()
+                                .unwrap(),
+                        );
                     }
                 }
             }
 
-            let time_string = &line[..13]; // timestamp format in logs: 081111 111607
-
-            let timestamp = if time_string.starts_with("du") {
+            let timestamp = if &line[..2] == b"du" {
                 // last few lines in log file do not have a timestamp
                 prev_timestamp // assume that the lines without timestamp occur at the same time as the logs before
             } else {
-                let datetime_str = format!(
-                    "20{}-{}-{} {}:{}:{}",
-                    &time_string[0..2],   // year
-                    &time_string[2..4],   // month
-                    &time_string[4..6],   // day
-                    &time_string[7..9],   // hour
-                    &time_string[9..11],  // minute
-                    &time_string[11..13]  // second
-                );
-                let naive_datetime =
-                    NaiveDateTime::parse_from_str(&datetime_str, "%Y-%m-%d %H:%M:%S").unwrap();
-                let datetime = Utc.from_utc_datetime(&naive_datetime).timestamp();
+                // timestamp format in logs: 081111 111607
+                let year = 2000 + i32::from_radix_10(&line[0..2]).0;
+                let month = u32::from_radix_10(&line[2..4]).0;
+                let day = u32::from_radix_10(&line[4..6]).0;
+                let hour = u32::from_radix_10(&line[7..9]).0;
+                let minute = u32::from_radix_10(&line[9..11]).0;
+                let second = u32::from_radix_10(&line[11..13]).0;
+
+                let datetime = NaiveDate::from_ymd_opt(year, month, day)
+                    .unwrap()
+                    .and_hms_opt(hour, minute, second)
+                    .unwrap()
+                    .and_utc()
+                    .timestamp();
                 prev_timestamp = datetime;
                 datetime
             };
 
             let blk_id = blk_id.expect("Block ID not found");
-            let label = labels.get(blk_id).expect("Label not found");
+            let blk_id_num = blk_to_i(blk_id);
+            let label = labels.get(&blk_id_num).expect("Label not found");
 
-            let out_line = format!(
-                "{};{};{};{:.1};{}\n",
-                line_id, template_id, blk_id, timestamp as f64, label
-            );
+            let out_line = format!("{};{};", line_id, template_id);
             w.write_all(out_line.as_bytes()).unwrap();
+            w.write_all(blk_id).unwrap();
+            let out_line = format!(";{:.1};", timestamp as f64);
+            w.write_all(out_line.as_bytes()).unwrap();
+            w.write_all(decode_label(label)).unwrap();
+            w.write_all(b"\n").unwrap();
 
             for extra_blk_id in extra_blk_ids.iter() {
-                let label = labels.get::<str>(extra_blk_id).expect("Label not found");
-                let out_line = format!(
-                    "{};{};{};{:.1};{}\n",
-                    line_id, template_id, extra_blk_id, timestamp as f64, label
-                );
+                let blk_id_num = blk_to_i(extra_blk_id);
+                let label = labels.get(&blk_id_num).expect("Label not found");
+
+                let out_line = format!("{};{};", line_id, template_id);
                 w.write_all(out_line.as_bytes()).unwrap();
+                w.write_all(extra_blk_id).unwrap();
+                let out_line = format!(";{:.1};", timestamp as f64);
+                w.write_all(out_line.as_bytes()).unwrap();
+                w.write_all(decode_label(label)).unwrap();
+                w.write_all(b"\n").unwrap();
             }
         }
         w.flush().unwrap();
@@ -152,10 +221,10 @@ fn main() {
 
 // The output is wrapped in a Result to allow matching on errors.
 // Returns an Iterator to the Reader of the lines of the file.
-fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+fn read_lines<P>(filename: P) -> io::Result<io::Split<io::BufReader<File>>>
 where
     P: AsRef<Path>,
 {
     let file = File::open(filename)?;
-    Ok(io::BufReader::new(file).lines())
+    Ok(io::BufReader::new(file).split(b'\n'))
 }
